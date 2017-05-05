bb23ab4707a6e17602d79fef13b391b6c41e7058
Karamchi <jminces@ws15.labo2>
2017-05-05 20:01:57 -0300

    Me voy a la chota

--- queries.sql
+++ queries.sql
@@ -2,102 +2,82 @@
 
 select mcid.ncg, mcid.nombreModalidad, mcid.IDcategoria
     from
-        competidor comp,
-        (select sde.nombreModalidad, sde.IDcategoria, i.ncg 
-            from SeDivideEn sde, inscripto i
-            where sde.nombreModalidad = i.nombreModalidad) as mcid,
-        categoria cat
-    where c.ncg = mcid.ncg
-        and cat.IDcategoria = mcid.IDcategoria
-        and comp.peso >= cat.pesoMin
-        and comp.peso <= cat.pesoMax
-        and comp.edad >= cat.edadMin
-        and comp.edad <= cat.edadMax
+        (inscripto i inner join seDivideEn sde on sde.nombreModalidad = i.nombreModalidad)
+	inner join categoria cat on cat.IDcategoria = sde.IDcategoria
+	inner join competidor comp on comp.numeroCertificadoGraduacion = i.numeroCertificadoGraduacion
+    where 
+        (cat.pesoMax is null or (comp.peso <= cat.pesoMax and comp.peso >= cat.pesoMin))
+        and (cat.edadMin is null or (and (comp.edad >= cat.edadMin and comp.edad <= cat.edadMax)))
         and comp.genero = cat.genero
         and comp.graduacion = cat.graduacion
 
--- chequear null
 -- devolver equipos en vez de personas?
 
 -- El país que obtuvo mayor cantidad de medallas de oro, plata y bronce.
 
---set resultadosPais = 
---    (select m.nombrepais, mr.resultado
---       from m maestro,
---        (select p.resultado, c.certificadoGraduacion
---            from (participacionIndividual pi inner join (participaciondeEquipo pe inner join competidor c)), participacion p
---            where (p.tipo = participacionDeEquipo 
---                   and p.idParticipacion = pe.idParticipacion 
---                   and c.nombreEquipo = pe.nombreEquipo)
---                  or (p.tipo = participacionIndividual 
---                      and c.certificadoGraduacion = pi.certificadoGraduacion
---                      and e.certificadoGraduacion = c.certificadoGraduacion) 
---        as cr)
---        where m.placaInstructor = cr.placaInstructor) 
---    as mr)
-
-set resultadosPais =
-	select sum(p.resultado), ep.nombrePais
+set resultadosMaestro =
+	select p.resultado, em.numeroPlacaInstructor
 	from (participacion p INNER JOIN participacionEquipo e on p.idParticipacion = e.idParticipacion)
-		INNER JOIN equiposPais ep on ep.nombreEquipo = e.nombreEquipo
-	where p.resultado <= 3
-	group by ep.nombrePais
+		INNER JOIN equiposMaestro em on em.nombreEquipo = e.nombreEquipo
 	union
-	select sum(p.resultado), m.nombrePais
+	select p.resultado, m.numeroPlacaInstructor
 	from ((participacion p INNER JOIN participacionIndividual i on p.idParticipacion = i.idParticipacion)
 		INNER JOIN competidor c on i.numeroCertificadoGraduacion = c.numeroCertificadoGraduacion)
-		INNER JOIN maestro m on c.placaInstructor = m.placaInstructor
-	where p.resultado <= 3
-	group by m.nombrepais
+		INNER JOIN maestro m on c.placaInstructor = m.placaInstructor;
 
-select sum(resultado), nombrePais
-from resultadosPais
-group by nombrePais
-order by sum(resultado)
-limit 1
+set maestroMedallas =
+select m.nombrePais from
+(select count(resultadosMaestro.resultado) as bronce, resultadosMaestro.placaInstructor
+	from resultadosMaestro
+	where resultadosMaestro.resultado = 3
+	group by resultadosMaestro.placaInstructor) as maestroBronce
+inner join 
+(select count(resultadosMaestro.resultado) as plata, resultadosMaestro.placaInstructor
+	from resultadosPais
+	where resultadosMaestro.resultado = 2
+	group by resultadosMaestro.placaInstructor) as maestroPlata)
+on maestrobronce.nombrePais = maestroPlata.nombrePais
+inner join
+(select count(resultadosMaestro.resultado) as oro, resultadosMaestro.placaInstructor
+	from resultadosPais
+	where resultadosMaestro.resultado = 1
+	group by resultadosMaestro.placaInstructor) as maestroOro
+on paisOro.nombrePais = paisPlata.nombrePais;
 
---select nombrePais, oro, plata, bronce
---    from
---(select nombrePais, sum(case when resultado = 1 then 1 else null end) as oro, sum(case when resultado = 2 then 1 else null end) as plata, sum(case when resultado = 3 then 1 else null end) as bronce
---from resultadosPais
---group by nombrePais)  -- Aca hay algo raro, tiene q ser null o 0 ?
+set paisMedallas =
+select m.nombrePais, sum(maestroMedallas.bronce) as bronce, sum(maestroMedallas.plata) as plata, sum(maestroMedallas.oro) as oro
+from maestroMedallas INNER JOIN maestro on maestroMedallas.placaInstructor = maestro.placaInstructor
+	group by maestro.nombrePais;
 
-
--- falta agarrar el max. Además creo que estoy contando una medalla por integrante de equipo.
+select * from (paisMedallas
+	order by oro desc
+	limit 1
+    union
+	paisMedallas
+	order by plata desc
+	limit 1
+    union
+	paisMedallas
+	maestroMedallas INNER JOIN maestro on maestroMedallas.placaInstructor = maestro.placaInstructor
+	order by bronce desc
+	limit 1);
 
 -- El medallero por escuela.
 
-set resultadosEscuela = 
-(select m.escuela, mr.resultado
-    from m maestro,
-    (select p.resultado, c.certificadoGraduacion
-        from participacionIndividual pi, participaciondeEquipo pe, competidor c, participacion p
-        where (p.tipo = participacionDeEquipo 
-              and p.idParticipacion = pe.idParticipacion 
-              and c.nombreEquipo = pe.nombreEquipo)
-            or (p.tipo = participacionIndividual 
-                and c.certificadoGraduacion = pi.certificadoGraduacion
-                and e.certificadoGraduacion = c.certificadoGraduacion) 
-    as cr)
-    where m.placaInstructor = cr.placaInstructor) as mr
-
-select escuela, sum(case when resultado = 1 then 1 else null end) as oro, sum(case when resultado = 2 then 1 else null end) as plata, sum(case when resultado = 3 then 1 else null end) as bronce
-from resultadosEscuela
-group by escuela
-order by oro desc
+select m.escuela, sum(maestroMedallas.bronce) as bronce, sum(maestroMedallas.plata) as plata, sum(maestroMedallas.oro) as oro
+from maestroMedallas INNER JOIN maestro on maestroMedallas.placaInstructor = maestro.placaInstructor
+	group by maestro.escuela;
 
 -- Sabiendo que las medallas de oro suman 3 puntos, las de plata 2 y las de bronce 1
 -- punto, se quiere realizar un ranking de puntaje por país y otro por escuela.
 
-select nombrePais, sum(case when resultado = 1 then 3 else null end) + sum(case when resultado = 2 then 2 else null end) + sum(case when resultado = 3 then 1 else null end) as puntaje
-    from resultadosPais
-    group by nombrePais
-    order by puntaje desc;
+select nombrePais, oro*3 + plata*2 + bronce as puntaje
+from paisMedallas
+order by puntaje desc;
 
-select escuela, sum(case when resultado = 1 then 3 else null end) + sum(case when resultado = 2 then 2 else null end) + sum(case when resultado = 3 then 1 else null end) as puntaje
-    from resultadosEscuela
-    group by nombreEscuela
-    sort by puntaje desc
+select escuela, oro*3 + plata*2 + bronce as puntaje
+from escuelaMedallas
+order by puntaje desc;
 
 -- Dado un competidor, la lista de categorías donde haya participado y el resultado obtenido.
 
@@ -124,30 +104,19 @@ sort by nombrePais desc
 
 select a.placaArbitro
 from 
-    arbitraje a,
-    (select idJurado
-    from arbitradoPor
-    where nombreModalidad = 'combate' or nombreModalidad = 'combate por equipos') as jm
-where a.idJurado = jm.idJurado
+    arbitraje a INNER JOIN arbitradoPor ap ON a.idJurado = ap.idJurado
+where ap.nombreModalidad = 'combate' or ap.nombreModalidad = 'combate por equipos'
 and a.rol = 'arbitro central'
 
 -- La lista de equipos por país.
 
-select e.nombreEquipo, m.nombrePais 
-from e equipo
-	join c competidor
-		on e.nombreEquipo = c.nombreEquipo
-	join m maestro
-		on c.placaInstructor = m.placaInstructor
-sort by m.nombrePais desc
+set equiposMaestro =
 
---esa creo que repite una vez por competidor, otra:
-
-select c.nombreEquipo, min(m.pais) from 
-competidor c
-	join m maestro
-		on m.placaInstructor = c.placaInstructor
+select c.nombreEquipo, min(m.placaInstructor) 
+from competidor c inner join m maestro on m.placaInstructor = c.placaInstructor
 where c.nombreEquipo is not null
-group by c.nombreEquipo
+group by c.nombreEquipo; --reusado más arriba
 
---eso sería util para los medalleros si el min no fuera tan trucho
+select equiposMaestro.equipo, maestro.nombrePais
+from equiposMaestro INNER JOIN maestro on equiposMaestro.placaInstructor = maestro.placaInstructor
+order by maestro.nombrePais desc
