e3f123252e355ade12b6c4a5562df8d4fc0fade8
Karamchi <karamchi.ghandi@gmail.com>
2017-05-18 04:05:50 -0300

    Fixes y más tests

--- algunos_datos.sql
+++ algunos_datos.sql
@@ -65,17 +65,18 @@ insert into SeDivideEn
 insert into Pais values("Sealand");
 
 -- (PlacaInstructor, Escuela, NombreCompleto, Graduacion, NombrePais)
-insert into Maestro values(1, "Escuela 1", "Maestro", 1, "Sealand");
+insert into Maestro values(1, "Escuela 1", "Maestro 1", 1, "Sealand");
+insert into Maestro values(2, "Escuela 2", "Maestro 2", 1, "Abjasia");
 
 -- (NumeroCertificadoGraduacion, Foto, Graduacion, NombreCompleto, PlacaInstructor)
 insert into Registrado values(1, null, 1, "A A", 1);
-insert into Registrado values(2, null, 2, "B B", 1);
+insert into Registrado values(2, null, 2, "B B", 2);
 insert into Registrado values(3, null, 3, "C C", 1);
-insert into Registrado values(4, null, 4, "D D", 1);
+insert into Registrado values(4, null, 4, "D D", 2);
 insert into Registrado values(5, null, 5, "E E", 1);
-insert into Registrado values(6, null, 6, "F F", 1);
+insert into Registrado values(6, null, 6, "F F", 2);
 insert into Registrado values(7, null, 1, "G G", 1);
-insert into Registrado values(8, null, 1, "H H", 1);
+insert into Registrado values(8, null, 1, "H H", 2);
 
 -- (NumeroCertificadoGraduacion, Peso, DNI, FechaNacimiento, Sexo, RolEquipo, NombreEquipo)
 insert into Competidor values (1, 60, 12345671, "1994-05-06", "M", null, null);
@@ -106,3 +107,65 @@ insert into ParticipacionIndividual values(1, 1);
 insert into ParticipacionIndividual values(2, 2);
 insert into ParticipacionIndividual values(3, 3);
 
+-- Fin test medallero individual
+
+-- (ID, Graduacion, NombreCompleto, NombrePais)
+insert into Arbitro values(1, 6, "Arbitro 1", "Imperio Romano");
+insert into Arbitro values(2, 6, "Arbitro 2", "Imperio Romano");
+insert into Arbitro values(3, 6, "Arbitro 3", "Imperio Romano");
+insert into Arbitro values(4, 6, "Arbitro 4", "Imperio Romano");
+insert into Arbitro values(5, 6, "Arbitro 5", "Imperio Romano");
+insert into Arbitro values(6, 6, "Arbitro 6", "Imperio Romano");
+insert into Arbitro values(7, 6, "Arbitro 7", "Imperio Romano");
+
+insert into Jurado values(1);
+
+-- (IDJurado, IDArbitro, rol)
+insert into Arbitraje values(1, 1, "arbitro central");
+insert into Arbitraje values(1, 2, "presidente de mesa");
+insert into Arbitraje values(1, 3, "juez");
+insert into Arbitraje values(1, 4, "juez");
+insert into Arbitraje values(1, 5, "suplente");
+insert into Arbitraje values(1, 6, "suplente");
+insert into Arbitraje values(1, 7, "suplente");
+
+-- (Modalidad, IDJurado, Ring)
+insert into ArbitradoPor values("Combate", 1, 1);
+
+-- Fin test árbitros
+
+insert into Equipo values("Los mas mejores");
+
+-- (NumeroCertificadoGraduacion, Foto, Graduacion, NombreCompleto, PlacaInstructor)
+insert into Registrado values(9, null, 5, "Nueve", 2);
+insert into Registrado values(10, null, 5, "Diez", 2);
+insert into Registrado values(11, null, 5, "Once", 2);
+insert into Registrado values(12, null, 5, "Doce", 2);
+insert into Registrado values(13, null, 5, "Trece", 2);
+insert into Registrado values(14, null, 5, "Catorce", 2);
+insert into Registrado values(15, null, 5, "Quince", 2);
+insert into Registrado values(16, null, 5, "Dieciseis", 2);
+
+-- (NumeroCertificadoGraduacion, Peso, DNI, FechaNacimiento, Sexo, RolEquipo, NombreEquipo)
+insert into Competidor values (9, 60, 12345691, "1995-05-06", "F", "Titular", "Los mas mejores");
+insert into Competidor values (10, 70, 12345692, "1995-05-06", "F", "Titular", "Los mas mejores");
+insert into Competidor values (11, 80, 12345693, "1995-05-06", "F", "Titular", "Los mas mejores");
+insert into Competidor values (12, 90, 12345694, "1995-05-06", "F", "Titular", "Los mas mejores");
+insert into Competidor values (13, 60, 12345695, "1995-05-06", "F", "Titular", "Los mas mejores");
+insert into Competidor values (14, 60, 12345696, "1995-05-06", "F", "Suplente", "Los mas mejores");
+insert into Competidor values (15, 60, 12345697, "1995-05-06", "F", "Suplente", "Los mas mejores");
+insert into Competidor values (16, 60, 12345608, "1995-05-06", "F", "Suplente", "Los mas mejores");
+
+insert into Inscripto values (9, "Combate por Equipos");
+insert into Inscripto values (10, "Combate por Equipos");
+insert into Inscripto values (11, "Combate por Equipos");
+insert into Inscripto values (12, "Combate por Equipos");
+insert into Inscripto values (13, "Combate por Equipos");
+insert into Inscripto values (14, "Combate por Equipos");
+insert into Inscripto values (15, "Combate por Equipos");
+insert into Inscripto values (16, "Combate por Equipos");
+
+-- (IDParticipacion, Resultado, IDCategoria, NombreModalidad, NumeroCertificadoGraduacionCoach)
+insert into Participacion values(4, 1, 8, "Combate por Equipos", "ParticipacionDeEquipo");
+
+insert into ParticipacionDeEquipo values(4, "Los mas mejores");
--- queries.sql
+++ queries.sql
@@ -5,16 +5,17 @@ FROM Inscripto i
 INNER JOIN SeDivideEn sde on sde.NombreModalidad = i.NombreModalidad
 INNER JOIN Categoria cat on cat.IDCategoria = sde.IDCategoria
 INNER JOIN Competidor comp on comp.NumeroCertificadoGraduacion = i.NumeroCertificadoGraduacion
+INNER JOIN Registrado r on r.NumeroCertificadoGraduacion = i.NumeroCertificadoGraduacion
 WHERE
   ((cat.PesoMaximo is null and cat.PesoMinimo is null)
     or (cat.PesoMaximo is null and comp.Peso >= cat.PesoMinimo)
     or (comp.Peso < cat.PesoMaximo and comp.Peso >= cat.PesoMinimo)
     or (comp.Peso < cat.PesoMaximo and cat.PesoMinimo is null))
-  and ((cat.EdadMinima is null and (FLOOR(DATEDIFF(DAY, comp.FechaNacimiento, GETDATE()) / 365.25) < cat.EdadMaxima)
-      or ((FLOOR(DATEDIFF(DAY, comp.FechaNacimiento, GETDATE()) / 365.25) >= cat.EdadMinima and FLOOR(DATEDIFF(DAY, comp.FechaNacimiento, GETDATE()) / 365.25) < cat.EdadMaxima)))
+    and ((cat.EdadMinima is null and (FLOOR(DATEDIFF(NOW(), comp.FechaNacimiento) / 365.25) < cat.EdadMaxima)
+      or ((FLOOR(DATEDIFF(NOW(), comp.FechaNacimiento) / 365.25) >= cat.EdadMinima and FLOOR(DATEDIFF(NOW(), comp.FechaNacimiento) / 365.25) < cat.EdadMaxima)))
   and comp.Sexo = cat.Sexo
-  and (cat.Graduacion is null or comp.Graduacion = cat.Graduacion)
-  and i.NombreModalidad != 'Combate por Equipos'
+  and (cat.Graduacion is null or r.Graduacion = cat.Graduacion)
+  and i.NombreModalidad != 'Combate por Equipos')
 order by i.NombreModalidad, cat.IDCategoria;
 
 -- Equipos por categoria
@@ -24,9 +25,10 @@ FROM Inscripto i
 INNER JOIN SeDivideEn sde on sde.NombreModalidad = i.NombreModalidad
 INNER JOIN Categoria cat on cat.IDCategoria = sde.IDCategoria
 INNER JOIN Competidor comp on comp.NumeroCertificadoGraduacion = i.NumeroCertificadoGraduacion
+INNER JOIN Registrado r on r.NumeroCertificadoGraduacion = i.NumeroCertificadoGraduacion
 WHERE
-  and comp.Sexo = cat.Sexo
-  and (cat.Graduacion is null or comp.Graduacion = cat.Graduacion)
+  comp.Sexo = cat.Sexo
+  and (cat.Graduacion is null or r.Graduacion = cat.Graduacion)
   and comp.NombreEquipo is not null
   and i.NombreModalidad != 'Combate por Equipos'
 group by comp.NombreEquipo
@@ -34,60 +36,52 @@ order by cat.IDCategoria;
 
 -- El país que obtuvo mayor cantidad de medallas de oro, plata y bronce.
 
-create view EquiposMaestro as
-SELECT c.NombreEquipo, min(m.PlacaInstructor)
-FROM Competidor c INNER JOIN m Maestro on m.PlacaInstructor = c.PlacaInstructor
+create or replace view EquiposMaestro as
+SELECT c.NombreEquipo, min(m.PlacaInstructor) as PlacaInstructor
+FROM Competidor c
+	INNER JOIN Registrado r on c.NumeroCertificadoGraduacion = r.NumeroCertificadoGraduacion
+	INNER JOIN Maestro m on m.PlacaInstructor = r.PlacaInstructor
 WHERE c.NombreEquipo is not null
 GROUP BY c.NombreEquipo;
 
-create view ResultadosMaestro as
+create or replace view ResultadosMaestro as
 	SELECT p.Resultado, em.PlacaInstructor
-	FROM (Participacion p INNER JOIN ParticipacionEquipo e on p.IDParticipacion = e.IDParticipacion)
+	FROM (Participacion p INNER JOIN ParticipacionDeEquipo e on p.IDParticipacion = e.IDParticipacion)
 		INNER JOIN EquiposMaestro em on em.NombreEquipo = e.NombreEquipo
 	union
 	SELECT p.Resultado, m.PlacaInstructor
-	FROM ((Participacion p INNER JOIN ParticipacionIndivIDual i on p.IDParticipacion = i.IDParticipacion)
-		INNER JOIN Competidor c on i.NumeroCertificadoGraduacionCompetidor = c.NumeroCertificadoGraduacion)
-		INNER JOIN Maestro m on c.PlacaInstructor = m.PlacaInstructor;
+	FROM ((Participacion p INNER JOIN ParticipacionIndividual i on p.IDParticipacion = i.IDParticipacion)
+		INNER JOIN Registrado r on i.NumeroCertificadoGraduacionCompetidor = r.NumeroCertificadoGraduacion)
+		INNER JOIN Maestro m on r.PlacaInstructor = m.PlacaInstructor;
 
-create view MaestroMedallas as SELECT * FROM
-(SELECT ResultadosMaestro.PlacaInstructor, count(ResultadosMaestro.Resultado) as bronce
-	FROM ResultadosMaestro
-	WHERE ResultadosMaestro.Resultado = 3
-	GROUP BY ResultadosMaestro.PlacaInstructor) as MaestroBronce
-INNER JOIN
-(SELECT ResultadosMaestro.PlacaInstructor, count(ResultadosMaestro.Resultado) as plata
-	FROM ResultadosMaestro
-	WHERE ResultadosMaestro.Resultado = 2
-	GROUP BY ResultadosMaestro.PlacaInstructor) as MaestroPlata
-on Maestrobronce.PlacaInstructor = MaestroPlata.PlacaInstructor
-INNER JOIN
-(SELECT ResultadosMaestro.PlacaInstructor, count(ResultadosMaestro.Resultado) as oro
-	FROM ResultadosMaestro
-	WHERE ResultadosMaestro.Resultado = 1
-	GROUP BY ResultadosMaestro.PlacaInstructor) as MaestroOro
-on MaestroOro.PlacaInstructor = MaestroPlata.PlacaInstructor;
+create or replace view MaestroMedallas as
+SELECT ResultadosMaestro.PlacaInstructor, 
+	sum(CASE WHEN ResultadosMaestro.Resultado = 3 THEN 1 ELSE 0 END) as bronce,
+	sum(CASE WHEN ResultadosMaestro.Resultado = 2 THEN 1 ELSE 0 END) as plata,
+	sum(CASE WHEN ResultadosMaestro.Resultado = 1 THEN 1 ELSE 0 END) as oro
+	FROM ResultadosMaestro;
 
-create view paisMedallas as
+create or replace view paisMedallas as
 SELECT m.NombrePais, sum(MaestroMedallas.bronce) as bronce, sum(MaestroMedallas.plata) as plata, sum(MaestroMedallas.oro) as oro
 FROM MaestroMedallas INNER JOIN Maestro on MaestroMedallas.PlacaInstructor = Maestro.PlacaInstructor
 	GROUP BY Maestro.NombrePais;
 
 SELECT * FROM paisMedallas
-	ORDER BY oro desc
-	limit 1
-union select * from paisMedallas
-	ORDER BY plata desc
-	limit 1
-union select * from paisMedallas
-	ORDER BY bronce desc
-	limit 1;
+	where oro = (select max(oro) from paisMedallas)
+union
+SELECT * FROM paisMedallas
+	where plata = (select max(plata) from paisMedallas)
+union
+SELECT * FROM paisMedallas
+	where bronce = (select max(bronce) from paisMedallas);
 
 -- El medallero por Escuela.
-create view EscuelaMedallas as
+create or replace view EscuelaMedallas as
 SELECT Maestro.Escuela, sum(MaestroMedallas.bronce) as bronce, sum(MaestroMedallas.plata) as plata, sum(MaestroMedallas.oro) as oro
 FROM MaestroMedallas INNER JOIN Maestro on MaestroMedallas.PlacaInstructor = Maestro.PlacaInstructor
 	GROUP BY Maestro.Escuela;
+
+select * from EscuelaMedallas
 
 -- Sabiendo que las medallas de oro suman 3 puntos, las de plata 2 y las de bronce 1
 -- punto, se quiere realizar un ranking de puntaje por país y otro por Escuela.
@@ -109,7 +103,7 @@ CREATE PROCEDURE `participaciones_competidor`(
 ) BEGIN
     SELECT p.IDCategoria, p.Resultado
     FROM ParticipacionIndividual pi INNER JOIN Participacion p ON p.IDParticipacion = pi.IDParticipacion
-    WHERE pi.NumeroCertificadoGraduacionComptidor = NumeroCertificadoGraduacion
+    WHERE pi.NumeroCertificadoGraduacionCompetidor = NumeroCertificadoGraduacion
     UNION
     SELECT p.IDCategoria, p.Resultado
     FROM ParticipacionDeEquipo pe INNER JOIN Participacion p ON p.IDParticipacion = pe.IDParticipacion
@@ -123,7 +117,7 @@ DELIMITER ;
 -- El listado de los árbitros por país.
 
 SELECT *
-FROM arbitro
+FROM Arbitro
 ORDER BY NombrePais desc;
 
 -- La lista de todos los árbitros que actuaron como árbitro central en las modalidades de combate
@@ -131,11 +125,11 @@ ORDER BY NombrePais desc;
 SELECT a.PlacaArbitro
 FROM
     Arbitraje a INNER JOIN ArbitradoPor ap ON a.IDJurado = ap.IDJurado
-WHERE ap.NombreModalidad = 'Combate' or ap.NombreModalidad = 'Combate por Equipos'
+WHERE (ap.NombreModalidad = 'Combate' or ap.NombreModalidad = 'Combate por Equipos')
 and a.Rol = 'arbitro central';
 
 -- La lista de equipos por país.
 
-SELECT EquiposMaestro.Equipo, Maestro.NombrePais
+SELECT EquiposMaestro.NombreEquipo, Maestro.NombrePais
 FROM EquiposMaestro INNER JOIN Maestro on EquiposMaestro.PlacaInstructor = Maestro.PlacaInstructor
 ORDER BY Maestro.NombrePais desc;
